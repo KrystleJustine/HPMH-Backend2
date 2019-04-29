d319e4fc012dd06d512f11bcc516b376ed328bde
Krystle Justine <KrystleJustine@gmail.com>
2019-04-29 18:19:31 -0400

    Project Serializer showing linked tasks

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,15 @@
 class ProjectSerializer < ActiveModel::Serializer
-  has_many :tasks
   has_many :leads, through: :tasks
-  attributes :id, :name
+  attributes :id, :name, :tasks
+
+  def tasks
+    object.tasks.map do |task|
+      {
+        title:task.title,
+        lead: task.lead,
+        description:task.description,
+        status:task.status
+      }
+      end
+  end
 end
